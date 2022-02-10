59b1c6999c6a4164f92c32f56d91f022013996e0
Landon Schropp <schroppl@gmail.com>
2022-02-10 14:40:23 -0600

    Add Calendar.week

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -1,8 +1,17 @@
 module Calendar
 
+  # Returns a string representing the day.
+  # @param day The day to convert to a string.
   def day(day)
     day.to_s.rjust(2, " ")
   end
 
+  # Returns a string representing the week.
+  # @param days An array containing the days of the week. Empty days should be represented with nil.
+  def week(days)
+    days.map { day(_1) }.join(" ")
+  end
+
   module_function :day
+  module_function :week
 end
--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -4,6 +4,13 @@ describe Calendar do
 
   describe ".day" do
 
+    context "when the day is nil" do
+
+      it "returns an empty string representing the day" do
+        expect(Calendar.day(nil)).to eq("  ")
+      end
+    end
+
     context "when the day is a single digit" do
 
       it "returns the day as a string padded with a space" do
@@ -15,6 +22,30 @@ describe Calendar do
 
       it "returns the day as a string" do
         expect(Calendar.day(11)).to eq("11")
+      end
+    end
+  end
+
+  describe ".week" do
+
+    context "when the days array does not contain any empty days" do
+
+      it "returns all seven days of the week" do
+        expect(Calendar.week([ 1, 2, 3, 4, 5, 6, 7 ])).to eq(" 1  2  3  4  5  6  7")
+      end
+    end
+
+    context "when the days array is padded with nils at the beginning" do
+
+      it "substitutes spaces for the empty days" do
+        expect(Calendar.week([ nil, nil, nil, 1, 2, 3, 4 ])).to eq("          1  2  3  4")
+      end
+    end
+
+    context "when the days array is padded with nils at the end" do
+
+      it "substitutes spaces for the empty days" do
+        expect(Calendar.week([ 1, 2, 3, 4, nil, nil, nil ])).to eq(" 1  2  3  4         ")
       end
     end
   end
