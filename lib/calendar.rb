b6b970aa524f8ec584343fc9d09f1a0a3039f079
Landon Schropp <schroppl@gmail.com>
2022-02-10 15:17:11 -0600

    Add Calendar.days

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -1,3 +1,6 @@
+require "date"
+require "pry-byebug"
+
 module Calendar
 
   # Returns a string representing the day.
@@ -12,6 +15,24 @@ module Calendar
     days.map { day(_1) }.join(" ")
   end
 
+  # Returns the days in the month. This does not include the name of the month or the week header.
+  # @param date The date object represets the month.
+  def days(date)
+
+    # Use the start of the month.
+    date = Date.new(date.year, date.month, 1)
+
+    # Determine the number of days in the month.
+    days_in_month = Date.new(date.year, date.month, -1).day
+
+    # Print out the weeks for the month joined with a newline.
+    (-(date.wday - 1)..days_in_month).each_slice(7).to_a
+      .map { |week| week.map { _1 <= 0 ? nil : _1 } }
+      .map { week(_1) }
+      .join("\n")
+  end
+
   module_function :day
   module_function :week
+  module_function :days
 end
--- spec/calendar_spec.rb
+++ spec/calendar_spec.rb
@@ -49,4 +49,52 @@ describe Calendar do
       end
     end
   end
+
+  describe ".days" do
+
+    context "when the month begins on the first day of the week" do
+
+      it "does not include any spaces before the beginning of the month" do
+        expected = <<~CALENDAR.rstrip
+           1  2  3  4  5  6  7
+           8  9 10 11 12 13 14
+          15 16 17 18 19 20 21
+          22 23 24 25 26 27 28
+          29 30 31
+        CALENDAR
+
+        expect(Calendar.days(Date.parse("2021-08-01"))).to eq(expected)
+      end
+    end
+
+    context "when the provided date does not begin at the start of the month" do
+
+      it "does not include any spaces before the beginning of the month" do
+        expected = <<~CALENDAR.rstrip
+           1  2  3  4  5  6  7
+           8  9 10 11 12 13 14
+          15 16 17 18 19 20 21
+          22 23 24 25 26 27 28
+          29 30 31
+        CALENDAR
+
+        expect(Calendar.days(Date.parse("2021-08-03"))).to eq(expected)
+      end
+    end
+
+    context "when the month does not begin on the first day of the week" do
+
+      it "does not include any spaces before the beginning of the month" do
+        expected = <<~CALENDAR.rstrip
+                    1  2  3  4
+           5  6  7  8  9 10 11
+          12 13 14 15 16 17 18
+          19 20 21 22 23 24 25
+          26 27 28 29 30 31
+        CALENDAR
+
+        expect(Calendar.days(Date.parse("2021-12-01"))).to eq(expected)
+      end
+    end
+  end
 end
